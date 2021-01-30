560c5dad9666e1fade962b9effd3266f9428205c
Michael Hinkley <mhinkley42@gmail.com>
2021-01-30 16:28:34 -0700

    organize into new classes and update make file

---
+++ BitBoard.h
@@ -0,0 +1,26 @@
+#ifndef BITBOARD_H
+#define BITBOARD_H 
+
+
+#include "components.h"
+
+using namespace std;
+
+class BitBoard
+{
+	public:
+	
+	static long mask(int i);
+
+
+	BitBoard(Piece p, long i): piece(p), bb(i) {};
+
+	bool isOccupied(int i) const;
+
+	Piece piece;
+	long bb;
+};
+
+
+
+#endif 
